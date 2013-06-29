5ad7c7830c1238a6414c3d456a9474197c2ce0bd
Dustin McBride <dustin@chivve.com>
2013-06-28 22:52:29 -0700

    fixed barrets changes

--- index.php
+++ index.php
@@ -17,13 +17,18 @@
 					
 					<div class="row">
 
-					<div class="span6">
-						<img src="mbimages/boxofgoods.png" width="425" alt="Boxofgoods">
+				<div class="span6">
+						<!-- ><img src="mbimages/boxofgoods.png" width="425" alt="Boxofgoods"> -->
 					</div>
+
 						<div class="span6 heroinfo">
-							<h1>MakrBox™ is a monthly subscription of awesome goods delivered to you.</h1>
-							<p>Each month we send you incredibly useful and uniquely fun household goods created by our network of talented makers, craftsmen and designers. </p>
-							<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZQ43LS58F39N">	<button class="btn btn-large theme" type="button">Subscribe Now<span>(only $29 per month)</span></button></a>
+							<h1>A monthly box of incredibly useful and uniquely fun household products created by talented makers, craftsmen and designers.</h1>
+							</BR>
+						</br>
+					</br>
+				</br>
+							<!--<p>Each month we send you  </p> -->
+							<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KZQ43LS58F39N">	<button class="btn btn-large theme" type="button">CLICK TO SUBSCRIBE<span>(only $29 per month)</span></button></a>
 						</div>
 					</div>
 				</div>
@@ -64,19 +69,19 @@
 				
 						<div class="span4">
 						<img src="../mbimages/pointer2.png" width="82" height="100" alt="Pointer2">
-						<h5>1. JOIN MAKRBOX </h5>
+						<h5>1. WE FIND </h5>
 					<p>
-						It's easy to join, just click 'subscribe' and become a MakrBox member.  
+				         Every month we find awesome, handcrafted, design-forward products and pack 2-3 of them in a box.  
 					</p></div>
 						<div class="span4">
 						<img src="../mbimages/source.png" width="100" height="100" alt="Source">
-						<h5>2. WE SOURCE</h5>
+						<h5>2. WE SHIP</h5>
 					<p>
-						Every month we work with our network of talented makers, craftsmens and designer to find awesome handcrafted design forward goods. 
+						Each month we ship this neat little box of amazing products directly to your home or office. 
 					</p></div>
 						<div class="span4"><img src="../mbimages/discover.png" width="100" height="100" alt="Discover"><h5>3. YOU DISCOVER COOL STUFF</h5>
 						<p>
-  							We send you unique and useful goods for your home or apartment. Keep them forever or give one away as a gift.  You will love getting our package every month!
+  						You get unique and useful goods for your home. Keep them forever or give one away as a gift!
 						</p></div>
 
 			</div>
@@ -94,7 +99,7 @@
 				<div class="row">
 					<div class="span12">
 
-						<h6 class="subhead theme"><strong>EXAMPLE ITEMS</strong></h6>
+						<h6 class="subhead theme"><strong>~ SEE WHAT'S INSIDE MAKRBOX ~</strong></h6>
 
 						<div class="tab-content row">
 
@@ -138,7 +143,7 @@
 									<img src="mbimages/examples/chopstick.jpg" width="400" height="400" alt="Tealight">
 									<div class="mask">
 										<h2>Chopstick Basket</h2>
-				                        <p>This basket is made of 100% repur- posed bamboo chopsticks. They're col- lected from restaurants, sanitized at high temperatures and dyed naturally with black tea. The basket can be used to hold vegetables, small items such as keys, or used as a colander for this sea- sons fresh cherries. When not being used, simply fold it and store in a drawer.</p>
+				                        <p>This basket is made of 100% repur-posed bamboo chopsticks. They're col- lected from restaurants, sanitized at high temperatures and dyed naturally with black tea. The basket can be used to hold vegetables, small items such as keys, or used as a colander for this sea- sons fresh cherries. When not being used, simply fold it and store in a drawer.</p>
 				                       
 									</div>
 								</div>
@@ -148,8 +153,8 @@
 								<div class="view view-thumb">
 								<img src="mbimages/examples/snackbag.jpg" width="400" height="400" alt="Snackbag">
 									<div class="mask">
-										<h2>Snack bag</h2>
-				                        <p>Looking for a better alternative to plastic baggies? Try this reusable eco friendly mini snack bag. It's made from organic unbleached muslin with NO chemicals. You'll never use plastic again.</p>
+										<h2>Pigeon Toe</h2>
+				                        <p></p>
 				                       
 									</div>
 								</div>
